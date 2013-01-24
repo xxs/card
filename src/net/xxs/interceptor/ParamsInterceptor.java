69d627e19a68e6e8803c6643c3b7995d7d22b73b
xxs <xiaoshi332@163.com>
2013-01-24 18:09:17 +0800

    ttt

---
+++ src/net/xxs/interceptor/ParamsInterceptor.java
@@ -0,0 +1,25 @@
+package net.xxs.interceptor;
+
+import net.xxs.action.api.BaseApiAction;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class ParamsInterceptor extends BaseApiAction implements Interceptor {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String intercept(ActionInvocation arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
